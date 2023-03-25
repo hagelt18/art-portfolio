ae2a321426a1fc622ed6229e160b577f9121b372
hagelt18 <hagelt18@gmail.com>
2023-03-25 17:29:57 -0500

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useCallback, useMemo, useState } from 'react';
 import './App.css';
 import GalleryPage from './pages/gallery/GalleryPage';
 import AboutPage from './pages/about/AboutPage';
@@ -9,37 +8,10 @@ import '@fontsource/roboto/700.css';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import ResponsiveAppBar from './Menu';
-// import {
-//   createBrowserRouter,
-//   redirect,
-//   RouterProvider,
-
-// } from "react-router-dom";
-
-import { Routes, Route, Outlet, Link, redirect, Navigate } from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  // const router = createBrowserRouter([
-  //   // {
-  //   //   path: "/",
-  //   //   component: <GalleryPage/>
-  //   // },
-  //   {
-  //     path: "/",
-  //     component: <div>HOME</div>
-  //   },
-  //   {
-  //     path: "shop",
-  //     component: <div>SHOP</div>
-  //   },
-  //   {
-  //     path: "about",
-  //     component: <div>ABOUT</div>
-  //   }
-  // ]);
-
-
+  
   return (
     <Box className="App">
       <Box className="App-Header"
@@ -53,14 +25,11 @@ function App() {
       </Box>
       <ResponsiveAppBar />
       <Box className="App-Content">
-        {/* <RouterProvider router={router} /> */}
         <Routes>
-          {/* <Route path="/" element={<GalleryPage />} /> */}
           <Route path="/" element={<Navigate to="/gallery"/>} />
           <Route path="/gallery" element={<GalleryPage />} />
           <Route path="/about" element={<AboutPage/>} />
-          {/* <Route path="/shop" element={<div>SHOP</div>} /> */}
-          <Route path="*" element={<div>NOT FOUND</div>} />
+          <Route path="*" element={<div>PAGE NOT FOUND</div>} />
         </Routes>
       </Box>
     </Box>
