9ab07913f37788f4e66af864cc3510a734f1a0ea
hagelt18 <hagelt18@gmail.com>
2025-03-04 20:48:30 -0600

    Update links (new tab)

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -16,7 +16,12 @@ function About() {
       >
       </Box>
       <Typography style={{textAlign: 'left'}}>
-        Tim Hagel is an artist originally from the Fargo-Moorhead area and currently based out of the twin cities area in Minnesota. His art style is mostly inspired by gross-out art popular in the late 80s and early 90s cartoons and video games. Tim was a featured <Link href={coreConGuestUrl}>Guest Artist at CoreCon 2023</Link>, creating all of the art for the "Fantastic Worlds" theme for that year. He has also done other freelance work such as creating the t-shirt design for <Link href={scottStoryTShirtUrl}>Scott Story</Link>, a local professional wrestler as well as some artwork for the comedy band "Kamikaze Snowmen".
+        Tim Hagel is an artist originally from the Fargo-Moorhead area and currently based out of the twin cities area in Minnesota. 
+        His art style is mostly inspired by gross-out art popular in the late 80s and early 90s cartoons and video games. 
+        Tim was a featured <Link target="_blank" rel="noopener" href={coreConGuestUrl}>Guest Artist at CoreCon 2023</Link>, 
+        creating all of the art for the "Fantastic Worlds" theme for that year. 
+        He has also done other freelance work such as creating the t-shirt design for <Link target="_blank" rel="noopener" href={scottStoryTShirtUrl}>Scott Story</Link>, 
+        a local professional wrestler as well as some artwork for the comedy band "Kamikaze Snowmen".
       </Typography>
     </Box>
   )
